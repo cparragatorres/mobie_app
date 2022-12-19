407d863e482abf3303ec8a7df0f6a0ed8dbffb63
Chris-ASUS <humbertpt14@gmail.com>
2022-12-19 02:05:24 -0500

    Creando estructura

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,35 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 14.0, vertical: 12.0),
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: height * 0.7,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(24.0),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                        "https://lumiere-a.akamaihd.net/v1/images/e9fa83c7242fb46fa962150a60301d4e_2764x4096_7a402a9f.jpeg?region=0,0,2764,4096",
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
