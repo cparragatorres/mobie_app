a27198845934c988a01692c5f528961f5b1a6857
Chris-ASUS <humbertpt14@gmail.com>
2023-01-10 04:20:47 -0500

    Diseño estatico de cartel con titulo y descripcion

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -47,24 +47,66 @@ class _HomePageState extends State<HomePage> {
                   physics: BouncingScrollPhysics(),
                     itemCount: movies.length,
                     itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        margin: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 14.0),
-                        width: double.infinity,
-                        height: height*0.7,
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.3),
-                              blurRadius: 12.0,
-                              offset: const Offset(4, 4),
+                      return ClipRRect(
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 14.0),
+                          width: double.infinity,
+                          height: height*0.7,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.3),
+                                blurRadius: 12.0,
+                                offset: const Offset(4, 4),
+                              ),
+                            ],
+                            borderRadius: BorderRadius.circular(24.0),
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                "http://image.tmdb.org/t/p/w500${movies[index]["poster_path"]}",
+                              ),
                             ),
-                          ],
-                          borderRadius: BorderRadius.circular(24.0),
-                          image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: NetworkImage(
-                              "http://image.tmdb.org/t/p/w500${movies[index]["poster_path"]}",
-                            ),
+                          ),
+                          child: Stack(
+                            children: [
+                              Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Container(
+                                  padding: EdgeInsets.all(10.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.black.withOpacity(0.9),
+                                    borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(22.0),
+                                      bottomRight: Radius.circular(22.0),
+                                    )
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text(
+                                        "AVATAR",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 15.0,
+                                          fontWeight: FontWeight.w700,
+                                        ),
+                                      ),
+                                      Text(
+                                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                                        maxLines: 4,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 13.0,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       );
