c03d2f856b2d1d33d4435568265d8b952d072a36
Chris-ASUS <humbertpt14@gmail.com>
2023-01-23 01:58:39 -0500

    API Services 2

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,25 +1,28 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:mobie_app/models/movie_models.dart';
 
 class APIService{
-  getMovies() async {
+  Future<List<MovieModel>> getMovies() async {
     Uri _uri = Uri.parse(
         "https://api.themoviedb.org/3/discover/movie?api_key=b023410500aafb2c79fe3179a1da5f64");
     http.Response response = await http.get(_uri);
     if (response.statusCode == 200) {
       // print(response.body);
       Map<String, dynamic> myMap = json.decode(response.body);
-      movies = myMap["results"];
+      List movies = myMap["results"];
 
       // movies.forEach((element) {
       //   moviesList.add(MovieModel.fromJson(element));
       // });
       // print(moviesList);
 
-      moviesList = movies.map<MovieModel>((e) => MovieModel.fromJson(e)).toList();
+      List<MovieModel>moviesList = movies.map<MovieModel>((e) => MovieModel.fromJson(e)).toList();
       // print(moviesList);
 
       // setState(() {}); eliminar setState
+      return moviesList;
     }
+    return [];
   }
 }
